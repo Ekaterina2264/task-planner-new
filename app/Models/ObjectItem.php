b9202b28b9a0fefaef7af033e0493fbe7f36b149
Ekaterina2264 <70864183+Ekaterina2264@users.noreply.github.com>
2026-07-20 15:20:06 +0500

    Add objects workspace

---
+++ app/Models/ObjectItem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ObjectItem extends Model
+{
+    protected $fillable = [
+        'work_object_id',
+        'section',
+        'title',
+        'is_completed',
+        'completed_at',
+        'created_by',
+    ];
+
+    protected $casts = [
+        'is_completed' => 'boolean',
+        'completed_at' => 'datetime',
+    ];
+
+    public function workObject()
+    {
+        return $this->belongsTo(WorkObject::class);
+    }
+}
